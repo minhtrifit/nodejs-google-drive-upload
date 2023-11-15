bc41aecb5885e5b78605be2e4e12695970729147
minhtrifit <minhtri.fit@gmail.com>
2023-11-15 20:15:25 +0700

    config middleware

--- src/routes/ggdrive.router.js
+++ src/routes/ggdrive.router.js
@@ -2,20 +2,7 @@ var express = require("express");
 var router = express.Router();
 const GgDriveController = require("../controllers/ggdrive.controller");
 
-const multer = require("multer");
-const { v4: uuidv4 } = require("uuid");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "src/uploads/");
-  },
-  filename: function (req, file, cb) {
-    const uid = uuidv4();
-    cb(null, uid + ".mp3");
-  },
-});
-
-const upload = multer({ storage: storage });
+const { upload } = require("../middlewares/multer");
 
 router.get("/", GgDriveController.getInitRouter);
 router.post("/", upload.single("file"), GgDriveController.handleUploadMedia);
